bd89aa25c5b6d1a82924a7c9c0c4e1a16d512ecc
Arpit27kr <arpit27kr@gmail.com>
2023-05-01 00:53:01 +0530

    add 3

---
+++ src/main/java/com/upes/devopsproj/DevopsprojApplication.java
@@ -0,0 +1,13 @@
+package com.upes.devopsproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsprojApplication.class, args);
+	}
+
+}
